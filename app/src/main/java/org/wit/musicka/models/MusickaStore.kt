802e68006118494c23ed62eda50d12af57ae15bc
20083633 <20083633@users.noreply.github.com>
2022-05-04 14:25:43 +0100

    Function Navigation Drawer

--- app/src/main/java/org/wit/musicka/models/MusickaStore.kt
+++
@@ -1,9 +0,0 @@
-package org.wit.musicka.models
-
-
-interface MusickaStore {
-    fun findAll(): List<MusickaModel>
-    fun create(musicka: MusickaModel)
-    fun update(musicka: MusickaModel)
-    fun delete(musicka: MusickaModel)
-}
